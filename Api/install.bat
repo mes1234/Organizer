7033d70ee2d2553c39578b8d5178dbe9f2dfc738
WK WK <witold.kepczynski@gmail.com>
2021-12-29 21:37:37 +0100

    wip

---
+++ Api/install.bat
@@ -0,0 +1,1 @@
+go get github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2   google.golang.org/protobuf/cmd/protoc-gen-go    google.golang.org/grpc/cmd/protoc-gen-go-grpc
